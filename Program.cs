d57e2702f58c8b68c6fc5e25e555f355060ab56e
Jason <wujpkimo@gmail.com>
2022-05-22 22:48:39 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿var form = new Form()
+{
+    FormBorderStyle = FormBorderStyle.None,
+    StartPosition = FormStartPosition.CenterScreen,
+    TopMost = true,
+    ControlBox = false,
+    ShowInTaskbar = false,
+    Opacity = 0.8,
+    Width = 0,
+    Height = 0
+};
+var lbl = new Label()
+{
+    Padding = new Padding(8),
+    AutoSize = true,
+    Font = new Font("微軟正黑體", 12, FontStyle.Bold),
+    BackColor = Color.Coral
+};
+form.Controls.Add(lbl);
+form.Show();
+var startX = form.Left;
+void ShowInfo(string message)
+{
+    lbl.Text = message;
+    form.Width = lbl.Width;
+    form.Height = lbl.Height;
+    form.Left = startX - form.Width / 2;
+    form.Refresh();
+}
+try
+{
+    ShowInfo("上傳中...");
+    var uploadedCount = SimulateUpload();
+    //Console.WriteLine($"已成功上傳{uploadedCount}筆");
+    //Thread.Sleep(2000);
+    //ShowMessage($"已成功上傳{uploadedCount}筆");
+    ShowInfo($"已成功上傳{uploadedCount}筆");
+    Thread.Sleep(2000);
+    form.Close();
+}
+catch (Exception ex)
+{
+    //Console.ForegroundColor = ConsoleColor.DarkRed;
+    //Console.WriteLine(ex.Message);
+    //Console.ReadLine();
+    ShowMessage(ex.Message);
+}
+
+void ShowMessage(string v)
+{
+    MessageBox.Show(v, "上傳作業");
+}
+
+int SimulateUpload()
+{
+    //模擬上傳作業
+    var rnd = new Random();
+    if (rnd.Next() % 3 == 1)
+        throw new ApplicationException("隨機模擬上傳失敗");
+    Thread.Sleep(1500 + rnd.Next(3000));
+    return rnd.Next(5) + 1;
+}
